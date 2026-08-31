c34927bc9ed147a71a67d6d0c57b37ab5403f626
Nicholas Hernandez <ni166728@ucf.edu>
2026-08-31 20:40:54 +0000

    yyyyyy

--- eustis_check.c
+++ eustis_check.c
@@ -3,12 +3,22 @@
 
 int main(int argc, char *argv[]){
     if(argc != 3)
-    {
-        
-        return 0;    
+    {    
+        printf("Usage: ./eustis_check <input_file> <key>\n");
+        printf("Error: expected exactly 2 arguments, received %d.\n", argc - 1);
+        return 1; 
     }
-    
-    
-    
+    printf("argc = %d\n", argc);
+    printf("argv[0] = %63s\n", argv[0]);
+    printf("argv[1] = %63s\n", argv[1]);
+    printf("argv[2] = %63s\n\n", argv[2]);
+    printf("Key: %63s\n\n", argv[2]);
 
-}
+    FILE *fptr;
+    char line[256];
+
+    fptr = fopen(argv[1], "r");
+    while (fgets(line, sizeof(line), fptr) != NULL)
+    {
+        printf("%s\")
+    }
