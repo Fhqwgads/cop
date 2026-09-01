c473e1adfc75d40c36c200e40724d84d3874ca7d
Nicholas Hernandez <ni166728@ucf.edu>
2026-09-01 05:40:45 +0000

    almost done

--- eustis_check.c
+++ eustis_check.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]){
     if(argc != 3)
@@ -9,16 +10,41 @@ int main(int argc, char *argv[]){
         return 1; 
     }
     printf("argc = %d\n", argc);
-    printf("argv[0] = %63s\n", argv[0]);
-    printf("argv[1] = %63s\n", argv[1]);
-    printf("argv[2] = %63s\n\n", argv[2]);
-    printf("Key: %63s\n\n", argv[2]);
+    printf("argv[0] = %s\n", argv[0]);
+    printf("argv[1] = %s\n", argv[1]);
+    printf("argv[2] = %s\n\n", argv[2]);
+    printf("Key: %s\n\n", argv[2]);
 
     FILE *fptr;
     char line[256];
+    int strLength = strlen(line);
+    int totalChar = 0;
+    int totalLines = 0;
 
     fptr = fopen(argv[1], "r");
+    if(fptr == NULL)
+    {
+        perror("Error: cannot open file ’%s’.", argv[1]);
+        return 1;
+    }
+    printf("File contents: \n");
     while (fgets(line, sizeof(line), fptr) != NULL)
     {
-        printf("%s\")
-    }
+        if(strLength > 0 && line[strLength - 1] == '\n')
+        {
+            line[strLength - 1] = '\0';
+        }
+        printf("%d: %s",totalLines + 1, line);
+        totalLines++;
+        for(int i = 0; line[i] != '\0'; i++)
+        {
+            if(isalpha(line[i]))
+            {
+                totalChar++;
+            }
+        }
+        
+    }   
+    printf("\n\nLines: %d", totalLines);
+    printf("\nLetters: %d\n", totalChar);
+}
