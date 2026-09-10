10cc01bd7c6c4d7dab037557e6733673f5b5e506
Nicholas Hernandez <ni166728@ucf.edu>
2026-09-10 00:20:15 +0000

    fixing segmentation faults lordy lordy

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -4,6 +4,7 @@ This program is written by: Nicholas Anthony Hernandez */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 typedef struct monster  
 { 
 char *name; 
@@ -41,8 +42,16 @@ void releaseMemory(monster** monsterList, int monsterCount, region** regionList,
 
 int main(void)
 {
+    int monsterCount = 0;
+    int regionCount = 0;
+    int trainerCount = 0;
+    monster** monsterList = readMonsters(&monsterCount);
+    region** regionList = readRegions(&regionCount, monsterList, monsterCount);
+    //trainer* trainers = loadTrainers(&trainerCount, regionList, regionCount);
 
+    
 }
+
 
 monster* makeMonster(char *name, char *element, int population)
 {
@@ -50,50 +59,48 @@ monster* makeMonster(char *name, char *element, int population)
     if(mon == NULL)
     {
         perror("malloc failed.");
-        return 1;
+        return NULL;
     }
-    mon->name = *name;
-    mon->element = *element;
+    mon->name = name;
+    mon->element = element;
     mon->population = population;
+    return mon;
 }
 
 monster** readMonsters(int *monsterCount)
 {
+    scanf("%d", monsterCount);   
     monster** monsterList = malloc(*monsterCount * sizeof(monster*));
     if (monsterList == NULL)
     {
         perror("malloc failed.");
-        return 1;
+        return NULL;
     }
     char name[50];
     char element[50];
-    int population;
+    int population = 0;
 
     for(int i = 0; i < *monsterCount; i++)
     {
         scanf("%49s", name);
-        int len = strlen(name) + 1;
-        char *nameP = malloc(len * sizeof(char));
+        char *nameP = malloc((strlen(name) + 1) * sizeof(char));
         if (nameP == NULL)
         {
             perror("malloc failed.");
-            return 1;
+            return NULL;
         }
 
-        strcpy(*nameP, name);
-         
+        strcpy(nameP, name);
         
         scanf("%49s", element);
-        len = strlen(element) + 1;
-        char *elementP = malloc(len * sizeof(char));
+        char *elementP = malloc((strlen(element) + 1) * sizeof(char));
         if (elementP == NULL)
         {
             perror("malloc failed.");
-            return 1;
+            return NULL;
         }
         
-        strcpy(*elementP, element);
-         
+        strcpy(elementP, element);
 
         scanf("%d", &population);
 
@@ -101,51 +108,58 @@ monster** readMonsters(int *monsterCount)
         {
             population = 1000000;
         }
-        monsterList[i] = makeMonster(*nameP, *elementP, population);
-
+        
+        monsterList[i] = makeMonster(nameP, elementP, population);
+        
     }
     return monsterList;
 }
 
 region** readRegions(int *countRegions, monster** monsterList, int monsterCount)
 {
-    scanf("%d", *countRegions);
-    region** regions = malloc(*countRegions * sizeof(region));
+    scanf("%d", countRegions);
+    region** regions = malloc(*countRegions * sizeof(region*));
     if(regions == NULL)
     {
         perror("malloc failed.");
-        return 1;
+        return NULL;
     }
     char name[50];
     char scanMon[50];
-    int k = 0;
+    int k;
     for(int i = 0; i < *countRegions; i++)
     {
+        regions[i] = malloc(sizeof(region));
         scanf("%49s", name);
         char *nameP = malloc((strlen(name) + 1) * sizeof(char));
         if (nameP == NULL)
         {
             perror("malloc failed.");
-            return 1;
+            return NULL;
         }
-        regions[i]->name = *nameP;
-        scanf("%d", regions[i]->monster_cnt);
+        regions[i]->name = nameP;
+
+        scanf("%d", &regions[i]->monster_cnt);
         for(int j = 0; j < regions[i]->monster_cnt; j++)
         {
+            regions[i]->monsters = malloc(regions[i]->monster_cnt*sizeof(monster*));
+            if (regions[i]->monsters == NULL)
+            {
+                perror("malloc failed.");
+                return NULL;
+                free(regions[i]->monsters);
+            }
             scanf("%49s", scanMon);
-            k = 0;
-            while(monsterList[k] != NULL && !strcmp(monsterList[k]->name, scanMon))
+            for (int k = 0; k < monsterCount; k++)
             {
-                k++;
+
+                if (strcmp(monsterList[k]->name, scanMon))
+                {
+                    regions[i]->monsters[j] = monsterList[k];
+                    regions[i]->total_population += regions[i]->monsters[j]->population;
+                }
             }
-            if (strcmp(monsterList[k]->name, scanMon))
-            {
-                regions[i]->monsters[j] = monsterList[k];
-            }else
-            {
-                printf("monster name could not be found.");
-                regions[i]->monsters[j] = monsterList[0];
-            }
+           
         }
 
     }
@@ -161,28 +175,29 @@ trainer* loadTrainers(int *trainerCount, region** regionList, int countRegions)
     if(trainers == NULL)
     {
         perror("malloc failed.");
-        return 1;
+        return NULL;
     }
     for(int i = 0; i<*trainerCount; i++)
     {
         scanf("%49s", trainers[i].name);
-        scanf("%d", trainers[i].visits->region_cnt);
+        scanf("%d", &trainers[i].visits->captures);
+        scanf("%d", &trainers[i].visits->region_cnt);
         for(int j = 0; j<trainers[i].visits->region_cnt; j++)
         {
             int k = 0;
             scanf("%49s", regionName);
-            while(regionList[k] != NULL && !strcmp(regionName, regionList[k]->name))
+            while(regionList[k] != NULL && regionName != regionList[k]->name)
             {
                 k++;
             }
-            if(strcmp(regionName, regionList[k]->name))
+            if(regionName == regionList[k]->name)
             {
                 trainers[i].visits->regions[j] = regionList[k];
             }
             else
             {
                 printf("region not found");
-                return 1;
+                return NULL;
             }
         }
         
@@ -193,13 +208,39 @@ trainer* loadTrainers(int *trainerCount, region** regionList, int countRegions)
 
 void processInputs(monster** monsterList, int monsterCount, region** regionList, int regionCount, trainer* trainerList, int trainerCount)
 {
-    int *monsterCount = 0;
-    int *regionCount = 0;
-    readMonsters(monsterCount);
-    readRegions(regionCount, monsterList, monsterCount)
-    
+    printf("%49s", trainerList[0].name);
 }
 
+/*void processCaptures(trainer* trainers, int trainerCount, region** regionList, int regionCount)
+{
+    for (int i = 0; i < trainerCount; i++)
+    {
+        for (int j = 0; j < trainers[i].visits->region_cnt; j++)
+        {
+            for (int k = 0; k < trainers[i].visits->captures; k++)
+            {
+                trainers[i].visits->regions[j]->total_population
+            }
+        }
+    }
+}*/
+
+int freeMonsterList(monster** monsterList, int monsterCount)
+{
+    for (int i = 0; i<monsterCount; i++)
+    {
+        monsterList[i];
+    }
+}
+
+int freeRegions(region** regionList, int regionCount)
+{
+    
+}
+int freeTrainers(trainer* trainerList, int trainerCount)
+{
+    
+}
 void releaseMemory(monster** monsterList, int monsterCount, region** regionList, int regionCount, trainer* trainerList, int trainerCount)
 {
 
